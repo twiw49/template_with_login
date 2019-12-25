1e96b61f781882ae8d301b2ea60606f4b3a3b779
twiw49 <twiw49@gmail.com>
2019-12-25 16:06:34 +0900

    for deploy

--- src/sw/base.js
+++ src/sw/base.js
@@ -27,7 +27,7 @@ self.addEventListener("activate", e => {
 });
 
 self.addEventListener("fetch", e => {
-  if (!(e.request.url.indexOf("http") === 0)) cacheWithNetworkFallback(e);
+  cacheWithNetworkFallback(e);
 });
 
 // CACHE STRATEGIES
@@ -45,9 +45,10 @@ const cacheWithNetworkFallback = e => {
       // Fallback
       return fetch(e.request).then(newResponse => {
         // Cache fetched response
-        caches
-          .open(CACHE_NAME)
-          .then(cache => cache.put(e.request, newResponse));
+        caches.open(CACHE_NAME).then(cache => {
+          console.log(e.request.url);
+          return cache.put(e.request, newResponse);
+        });
         return newResponse.clone();
       });
     })
