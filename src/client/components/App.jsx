2ce0df6e59521c5b3df65ce92315755a0a4cc543
twiw49 <twiw49@gmail.com>
2020-01-12 22:51:30 +0900

    for deploy

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -1,59 +1,51 @@
-// import React, { Fragment } from 'react';
-// import PropTypes from 'prop-types';
-// import { Route, Redirect, Switch, withRouter } from 'react-router-dom';
-// import { connect } from 'react-redux';
-// import { compose } from 'redux';
-// import { createGlobalStyle } from 'styled-components';
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { Route, Redirect, Switch, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { createGlobalStyle } from 'styled-components';
 
-// import Home from './Home';
-// import LandingPage from './LandingPage';
+import Home from './Home';
+import LandingPage from './LandingPage';
 
-// const GlobalStyle = createGlobalStyle`
-// body {
-//   margin: 0;
-//   padding: 0;
-// }
-// *:active, *:focus {
-//   outline: 0;
-//   -moz-outline-style: none;
-// }
-// `;
-
-// const LoadingComponent = () => <div />;
-
-// const App = ({ user, isLoading }) => {
-//   return (
-//     <Fragment>
-//       <Switch>
-//         <Route
-//           exact
-//           path="/"
-//           component={isLoading ? LoadingComponent : user ? Home : LandingPage}
-//         />
-//         <Redirect to="/" />
-//       </Switch>
-//       <GlobalStyle />
-//     </Fragment>
-//   );
-// };
-
-// App.propTypes = {
-//   user: PropTypes.object,
-//   isLoading: PropTypes.bool
-// };
-
-// export default compose(
-//   withRouter,
-//   connect(state => ({
-//     user: state.user,
-//     isLoading: state.isLoading
-//   }))
-// )(App);
-
-import React, { Component } from 'react';
-
-export default class App extends Component {
-  render() {
-    return <div>로딩</div>;
-  }
+const GlobalStyle = createGlobalStyle`
+body {
+  margin: 0;
+  padding: 0;
 }
+*:active, *:focus {
+  outline: 0;
+  -moz-outline-style: none;
+}
+`;
+
+const LoadingComponent = () => <div />;
+
+const App = ({ user, isLoading }) => {
+  return (
+    <Fragment>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          component={isLoading ? LoadingComponent : user ? Home : LandingPage}
+        />
+        <Redirect to="/" />
+      </Switch>
+      <GlobalStyle />
+    </Fragment>
+  );
+};
+
+App.propTypes = {
+  user: PropTypes.object,
+  isLoading: PropTypes.bool
+};
+
+export default compose(
+  withRouter,
+  connect(state => ({
+    user: state.user,
+    isLoading: state.isLoading
+  }))
+)(App);
